d00bba4911fb76085b582810e89e6d6b9d9073dc
TorGj <82477510+TorGj@users.noreply.github.com>
2022-05-05 16:57:26 +0200

    <li> at About

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,13 @@ const About = () => {
         <section className='about'>
             <h2>About Tor Gjerrestad</h2>
             <p>
-                Master of Science (Informatics) educated at University of Oslo (Norway). Born 1974. Hometown: Ålesund, Norway.
-                Experienced paraglider pilot, travels a lot in May, June and July to get to nice places to fly.
-                Father of two (2003 and 2007), and I'm still tougether with their wonderful mother :-)
-                I'm interested in electronics, physics, mathematics, and do some experimenting at my home lab.
-                I ride a Yamaha XT 600 (motorcycle) when possible, mostly up in the mountains.
+                Master of Science (Informatics) educated at University of Oslo (Norway).
+                <li>Born 1974. Hometown: Ålesund, Norway.</li>
+                <li>Experienced paraglider pilot, travels a lot in May, June and July to get to nice places to fly.</li>
+
+                <li>Father of two (2003 and 2007), and I'm still tougether with their wonderful mother :-)</li>
+                <li>I'm interested in electronics, physics, mathematics, and do some experimenting at my home lab.</li>
+                <li>I ride a Yamaha XT 600 (motorcycle) when possible, mostly up in the mountains.</li>
             </p>
         </section>
     );
