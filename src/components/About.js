f8541ce139f77a591bbaaf500d3891a26287d193
TorGj <82477510+TorGj@users.noreply.github.com>
2022-05-06 15:29:00 +0200

    </li> About

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <section className='about'>
             <h2>About Tor Gjerrestad</h2>
             <p>
-                <li><strong>Master of Science (Informatics)</strong> from University of Oslo (Norway).
+                <li><strong>Master of Science (Informatics)</strong> from University of Oslo (Norway).</li>
                 <li>Born 1974. Hometown: Ålesund, Norway.</li>
                 <li>Experienced paraglider pilot, travels a lot in May, June and July to reach nice places to fly.</li>
                 <li>Father of two (2003 and 2007), and I'm still with their wonderful mother :-)</li>
