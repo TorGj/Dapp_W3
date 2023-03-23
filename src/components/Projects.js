b3601fba0c052fcaf349583c7ce8c6d97178a8fa
TorGj <82477510+TorGj@users.noreply.github.com>
2023-03-23 16:30:07 +0100

    Update Projects.js
    Updated text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,13 +14,17 @@ const Projects = () => {
                 <div className="projects__card">
                     <h3>Experiments</h3>
                     <img src={experiments} alt="Experimens Developing Page" />
-                    <p>This is an early prototype of my cloud chamber,
-                    it is silent, light weight, cheap and instantly working. It
-                    is also with 4x viewable area compared to most other models.
-                    I also develop new experiments and demonstrations, including
-                    vacuum, liquid nitogen, high voltage, high current and high velocity. Sometimes
-                    a great deal of sound also happens, typically when experimenting width
-                    high pressure.
+                    <p> This is an early prototype of my cloud
+                        chamber, it is silent, light weight, 
+                        cheap and instantly working. I is also
+                        with 4x viewable area compared to 
+                        most other models. I also develop new 
+                        experiments and demonstrations, 
+                        including, vacuum, liquid nitogen, high 
+                        voltage, high current and high velocity. 
+                        Sometimes a great deal of sound also 
+                        happens, typically when experimenting 
+                        width high pressure.
                     </p>
 
                     <a href="https://hackaday.com/" target="_blank" rel="noreferrer" className="button">Hackaday</a>
@@ -42,17 +46,20 @@ const Projects = () => {
                         requires personal engagement.
                     </p>
 
-                    <a href="https://alesund.kommune.no/veg-vatn-og-avlop/vatn-og-avlop/" target="_blank" rel="noreferrer" className="button">Water & Sewage</a>
+                    <a href="https://alesund.kommune.no/veg-vatn-og-avlop/vatn-og-avlop/" target="_blank" rel="noreferrer" className="button">Water/Sewage</a>
                     <a href="https://en.wikipedia.org/wiki/SCADA" target="_blank" rel="noreferrer" className="button">SCADA</a>
                 </div>
 
                 <div className="projects__card">
                     <h3>Paragliding</h3>
                     <img src={pg} alt="Paragliding" />
-                    <p>
-                    I'm an cross country paraglider pilot. Ranked no.11 2021 in Norway. I've between
-                    paragliding since 2007. In 2013 I got my paraglider instructor license,
-                     and I've been lead instructor for several beginner courses. In my area there are mountains and fjords, quite spectacular scenery.
+                    <p> I'm an cross country paraglider pilot. 
+                        Ranked no.11 2021 in Norway. I've between
+                        paragliding since 2007. In 2013 I got my
+                        paraglider instructor license, and I've 
+                        been lead instructor for several beginner
+                        courses. In my area there are mountains 
+                        and fjords, quite spectacular scenery.
                     </p>
 
                     <a href="https://gjerrestad.no" target="_blank" rel="noreferrer" className="button">My Site</a>
@@ -60,13 +67,16 @@ const Projects = () => {
                 </div>
                 
                 <div className="projects__card">
-                    <h3>Prewious Work</h3>
+                    <h3>Previous Work</h3>
                     <img src={work} alt="Work Related Content" />
-                    <p>I'm a High School Science teacher. My subjects are mathematics,
-                    physics, informatics. I've also done private lessons for university students
-                    in mathematics. Experimenting on www at work. I've got a persoal Website, but I'm also
-                    developer, content manager and contributor to my paraglider club's site. I'd like
-                    some new areas to explore, so send me a message if you wold like to hire me.
+                    <p> I was a High School Science teacher. 
+                        My subjects was mathematics, physics, 
+                        informatics. I've also done private lessons for university
+                    students in mathematics. I've got a self made 
+                    persoal Website, but I'm also developer, content
+                    manager and contributor to my paraglider club's 
+                    site. I likes new areas to explore, so send me a 
+                    message if you like what I do.
                     </p>
 
                     <a href="https://www.akademiet.no/vgs/skolesteder/alesund/" target="_blank" rel="noreferrer" className="button">Akademiet</a>
