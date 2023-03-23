89bf3676283cfc7cf6594a8984536d358f825a3e
TorGj <82477510+TorGj@users.noreply.github.com>
2023-03-23 17:59:22 +0100

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,25 +5,28 @@ const Experience = () => {
 
             <ul>
                 <li>
-                    <i>Education</i> - <strong>University of Oslo</strong>, Graduated Master of Science in 2002
+                    <i>Education</i> - <strong> University of Oslo </strong>, Graduated Master of Science in 2002
                     <ul>
                         <li>Studied physics, 2 years</li>
                         <li>Studied mathematics, 2 years</li>
                         <li>Studied informatics, 4 years</li>
-                        <li>Completed course in pedagogics, 1 year </li>
+                        <li>Graduated pedagogics, 1 year </li>
                     </ul>
                 </li>
                 <li>
-                    <i>Work</i> - <strong>Teacher</strong>, 2018-2021
+                    <i>Work</i> - <strong> Automation Engineer </strong>, 2022-current
                     <ul>
-                        <li>Works as science teacher at high school</li>
+                        <li>Supervisory Control and Data Acquisition (SCADA)</li>
+                    <i>Work</i> - <strong>Teacher</strong>, 2002-2022
+                    <ul>
+                        <li>Science teacher at high school</li>
                         <li>Side projects: web site developer</li>
-                        <li>Programming: Python, Javascript, HTML, CSS</li>
+                        <li>Programming: Python, JavaScript, HTML, CSS</li>
                         <li>Building computers</li>
                     </ul>
                 </li>
                 <li>
-                    <i>Intrests</i> - <strong>Building equipment for physics experiments</strong>, 1993-present
+                    <i>Interests</i> - <strong> Building equipment for physics experiments</strong>, 1993-present
                     <ul>
                         <li>Built several cloud chambers</li>
                         <li>Created experiments to demonstrate how nothing behaves</li>
